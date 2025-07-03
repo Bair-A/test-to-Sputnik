c6eea8d9efd1bfb6ffb2049e33217399b08734c1
Aveev Bair <aveevb123@gmail.com>
2025-07-03 10:20:57 +0300

    feat: update Products component structure, add styling, and fix package.json name

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import styles from "./Products.module.scss";
 import ProductCard from "@/components/ProductCard";
 import { MOCK_ITEMS } from "@/constants/constants";
 import Header from "@/components/Header";
@@ -8,7 +9,7 @@ const Products = () => {
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className={styles.wrapper}>
         {MOCK_ITEMS.length > 0 &&
           MOCK_ITEMS.map((product) => (
             <ProductCard key={product.id} product={product} />
