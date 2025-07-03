f9dd4e94e4f259091deec3558f0a7659b746f19f
Aveev Bair <aveevb123@gmail.com>
2025-07-03 11:53:45 +0300

    feat: update quotes from double to single in constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,60 +1,60 @@
-import { Currency, ProductType } from "@/shared/types";
+import { Currency, ProductType } from '@/shared/types';
 
 export const MOCK_ITEMS: ProductType[] = [
   {
     id: 1,
-    title: "Чайник электрический",
-    origin: "Россия",
+    title: 'Чайник электрический',
+    origin: 'Россия',
     price: 34900,
     currency: Currency.RUB,
-    imageUrl: "https://picsum.photos/id/1/200/300",
+    imageUrl: 'https://picsum.photos/id/1/200/300'
   },
   {
     id: 2,
-    title: "Кофемашина",
-    origin: "Германия",
+    title: 'Кофемашина',
+    origin: 'Германия',
     price: 120000,
     currency: Currency.EUR,
-    imageUrl: "https://picsum.photos/id/2/200/300",
+    imageUrl: 'https://picsum.photos/id/2/200/300'
   },
   {
     id: 3,
-    title: "Наушники",
-    origin: "США",
+    title: 'Наушники',
+    origin: 'США',
     price: 7999,
     currency: Currency.USD,
-    imageUrl: "https://picsum.photos/id/3/200/300",
+    imageUrl: 'https://picsum.photos/id/3/200/300'
   },
   {
     id: 4,
-    title: "Смартфон",
-    origin: "Южная Корея",
+    title: 'Смартфон',
+    origin: 'Южная Корея',
     price: 450000,
     currency: Currency.RUB,
-    imageUrl: "https://picsum.photos/id/4/200/300",
+    imageUrl: 'https://picsum.photos/id/4/200/300'
   },
   {
     id: 5,
-    title: "Пылесос",
-    origin: "Япония",
+    title: 'Пылесос',
+    origin: 'Япония',
     price: 85000,
     currency: Currency.USD,
-    imageUrl: "https://picsum.photos/id/5/200/300",
+    imageUrl: 'https://picsum.photos/id/5/200/300'
   },
   {
     id: 6,
-    title: "Ноутбук",
-    origin: "США",
+    title: 'Ноутбук',
+    origin: 'США',
     price: 1500000,
     currency: Currency.RUB,
-    imageUrl: "https://picsum.photos/id/6/200/300",
+    imageUrl: 'https://picsum.photos/id/6/200/300'
   },
   {
     id: 7,
-    title: "Фитнес-браслет",
-    origin: "Китай",
+    title: 'Фитнес-браслет',
+    origin: 'Китай',
     price: 2999,
     currency: Currency.USD,
-    imageUrl: "https://picsum.photos/id/7/200/300",
-  },
+    imageUrl: 'https://picsum.photos/id/7/200/300'
+  }
 ];
